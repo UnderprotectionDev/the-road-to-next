21fadbd8275c9ed7f9cee3c6557b10b047618b40
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2024-12-10 12:24:42 +0300

    Enhance TicketItem component with delete functionality
    - Added a delete button to the TicketItem component, allowing users to remove tickets.
    - Integrated the LucideTrash icon for the delete action.
    - Adjusted the layout to conditionally render the delete button based on the ticket detail view.

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,10 +1,11 @@
 import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import { LucideSquareArrowOutUpRight } from "lucide-react";
+import { LucideSquareArrowOutUpRight, LucideTrash } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
@@ -19,6 +20,14 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
         <LucideSquareArrowOutUpRight className="size-4" />
       </Link>
     </Button>
+  );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <LucideTrash className="size-4" />
+      </Button>
+    </form>
   );
 
   return (
@@ -45,10 +54,9 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
           </span>
         </CardContent>
       </Card>
-
-      {isDetail ? null : (
-        <div className="flex flex-col gap-y-1">{detailButton}</div>
-      )}
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
     </div>
   );
 };
